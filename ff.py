9eaaa5f00461d1f9a37be9b03692657919f7fb61
Neo <89941741+MatrixBytes@users.noreply.github.com>
2022-05-02 20:58:28 +0200

    Update ff.py

--- ff.py
+++ ff.py
@@ -1,7 +1,6 @@
 import itertools
 import string    
 import time     
-from colorama import Fore
 
 data = {
     'h': 5,
@@ -37,12 +36,10 @@ def force(data, result, tasks, todo = 1):
                     
                     if len(tasks) == done:
                         did += 1
-                        print(f'{Fore.CYAN}[{Fore.LIGHTMAGENTA_EX}FF{Fore.CYAN}] {Fore.LIGHTMAGENTA_EX}Found a right formula! {op.join([str(x) for x in num])} [{did}/{todo}]')
+                        print(f'[FF] Found a right formula! {op.join([str(x) for x in num])} [{did}/{todo}]')
 
                         if did == todo:
                             return
-                    #else:
-                    #    print(f'{Fore.CYAN}[{Fore.LIGHTMAGENTA_EX}FF{Fore.CYAN}] {Fore.LIGHTMAGENTA_EX}Failed, only {done}/{len(tasks)} tests passed! {op.join([str(x) for x in num])}')
 
 if __name__ == "__main__":
-    force(data, result, tasks, todo = 100)
+    force(data, result, tasks, todo = 1000)
